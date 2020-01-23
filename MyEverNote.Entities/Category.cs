fc55b008d3dd9a9587374b3eace7d225bd17e4cb
Elif Furuncu <eliffuruncu93@gmail.com>
2020-01-23 16:35:05 +0300

    first commit

---
+++ MyEverNote.Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEverNote.Entities
+{
+    [Table("Categories")]
+    public class Category:EntityBase
+    {
+        [Required,StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        //başka bir class la ilişkili olduğu için virtual olarak tanımlandı.
+        public virtual List<Note> Notes { get; set; }
+        
+    }
+}
---
+++ MyeverNote.BusinessLayer/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyeverNote.BusinessLayer
+{
+   public class Test
+    {
+        public Test()
+        {
+            MyEverNote.DataAccessLayer.DatabaseContext db = new MyEverNote.DataAccessLayer.DatabaseContext();
+            db.EvernoteUsers.ToList();
+        }
+    }
+}
